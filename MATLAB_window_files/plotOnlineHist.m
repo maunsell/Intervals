c9586cddc65f728dbb87c757202b08e01cc135ef
John Maunsell <maunsell@hms.harvard.edu>
2010-07-16 17:52:48 -0400

    Lots of improvements to Matlab code

--- MATLAB_window_files/plotOnlineHist.m
+++ MATLAB_window_files/plotOnlineHist.m
@@ -30,10 +30,58 @@ holdV = [input.holdTimesMs{:}];
 successIx = strcmp(input.trialOutcomeCell, 'success');
 failureIx = strcmp(input.trialOutcomeCell, 'failure');
 ignoreIx = strcmp(input.trialOutcomeCell, 'ignore');
+nCorr = sum(successIx);
+nFail = sum(failureIx);
+nIg = sum(ignoreIx);
+holdStarts = [input.holdStartsMs{:}];
 
-%% 1 - total hold time histogram
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 1 - Performance Values
 
-axH = subplot(spSz{:}, 1);
+if length(holdStarts) > 2
+	holdTimeMS = input.holdTimesMs{end};
+	switch input.trialOutcomeCell{end}
+	case 'success'
+		outcomeString = 'correct';
+	case 'failure'
+		outcomeString = 'early';
+	case 'ignore'
+		outcomeString = 'failed'
+	end
+	axH = subplot(spSz{:}, 1);						% default axes are 0 to 1
+	set(axH, 'Visible', 'off');
+	text(0.00, 1.0, {'Subject:', 'Time working:', 'Reward volume:'});
+	text(0.60, 1.0, {sprintf('%d', input.subjectNum), ...
+		sprintf('%.0d m', ...
+		round((input.holdStartsMs{end} - input.holdStartsMs{1})/60000)),...
+		sprintf('%.1f s', ...
+		sum(cat(2,input.juiceTimesMsCell{:})) / 1000)});
+	text(0.00, 0.6, {'Trials:', 'Correct:', 'Early:', 'Failed:'});
+	text(0.40, 0.6, {sprintf('%d', numTrials), sprintf('%d', nCorr), ...
+				sprintf('%d', nFail), sprintf('%d', nIg)});
+	text(0.60, 0.6, {' ', sprintf('%.0f%%', nCorr / numTrials * 100.0), ...
+				sprintf('%.0f%%', nFail / numTrials * 100.0), ...
+				sprintf('%.0f%%', nIg / numTrials * 100.0)});
+	text(0, 0.3, {sprintf('Interval 1: %d - %d ms', input.react1TimeMS, ...
+				input.react1TimeMS + input.react1DurMS), ...
+				sprintf('Interval 2: %d - %d ms', input.react2TimeMS, ...
+						input.react2TimeMS + input.react2DurMS)});
+
+%	if strcmp(outcomeString, 'correct')
+%		if holdTimeMS > input.react2TimeMS && ...
+%							holdTimeMS <= input.react2TimeMS + input.react2DurMS
+%			text(0, 0.22, 'Hit interval 2');
+%		elseif holdTimeMS > input.react1TimeMS && ...
+%						holdTimeMS <= input.react1TimeMS + input.react1DurMS
+%			text(0, 0.22, 'Hit interval 2');
+%		end
+%	end
+end			
+		
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2 - total hold time histogram
+
+axH = subplot(spSz{:}, 2);
 maxFail = max(holdV(find(failureIx)));
 if maxFail > 2000
   maxX = 3000;
@@ -63,23 +111,23 @@ xLim = [0 maxX + 50];
 set(gca, 'XLim', [0 input.reactTimeMs]);
 yLim = get(gca, 'YLim');
 
-title(sprintf('mean hold %4.0f ms', mean([input.holdTimesMs{:}])));
+title(sprintf('median hold %4.0f ms', median([input.holdTimesMs{:}])));
 xlabel('time (ms)');
 
-if ~isempty(input.tooFastTimeMs)
-  yLim = get(gca, 'YLim');
-  plot(input.tooFastTimeMs * [1 1], yLim, 'k--');
-end
+plotIntervalLines(input, 'vertical');
 
-%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2 - react time CDF
 
-%% 2 - react time CDF
-axH = subplot(spSz{:},4);
+axH = subplot(spSz{:},5);
 cdfplot([input.holdTimesMs{:}]);
+grid off;
 set(gca, 'XLim', [0 input.reactTimeMs], 'YLim', [0 1]);
 xlabel('time (ms)');
 ylabel('hold times < time');
 title('hold density function');
+hold on;
+plotIntervalLines(input, 'vertical');
 
 %%%%%%%%%%%%%%%%
 
@@ -122,7 +170,7 @@ title('hold density function');
 %%%%%%%%%%%%%%%%
 
 %% 4 - smoothed perf curve
-axH = subplot(spSz{:},2);
+axH = subplot(spSz{:},3);
 hold on;
 %plot(smooth(double(successIx), ceil(nTrial/10), 'lowess'));
 plot(smooth1(double(successIx), 'gauss', [2], 3));
@@ -132,20 +180,12 @@ lH2 = plot(smooth1(double(successIx), 'gauss', [16], 32));
 set(lH2, 'Color', 'k', 'LineWidth', 2);
 ylabel('pct correct');
 set(gca, 'YLim', [0 1]);
-nCorr = sum(successIx);
-nFail = sum(failureIx);
-nIg = sum(ignoreIx);
-suptitle(sprintf('%03d: Correct %d (%4.1f%%), early %d (%4.1f%%), ign %d (%4.1f%%) total %d', ...
-                 input.subjectNum, ...
-                 nCorr, nCorr/numTrials*100, nFail, nFail/numTrials*100, ...
-                 nIg, nIg/numTrials*100, numTrials));
 
 %%%%%%%%%%%%%%%%
 
 %% 6 - trial length plot
-axH = subplot(spSz{:},6);
+axH = subplot(spSz{:},9);
 hold on;
-holdStarts = [input.holdStartsMs{:}];
 %pH=semilogy(diff(holdStarts)/1000);
 hSDiffsSec = diff(holdStarts)/1000;
 % make outliers a fixed value
@@ -166,12 +206,12 @@ ylabel('trial start time diff (s)');
 xLim = get(gca, 'XLim');
 lH = plot(xLim, 20*[1 1], '--k');
 set(gca,'YLim', [0 121]);
-%%%%%%%%%%%%%%%%
 
-%% 5 - trials per unit time
-axH = subplot(spSz{:},5);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 5 - trials per unit time
+
+axH = subplot(spSz{:},6);
 hold on;
-holdStarts = [input.holdStartsMs{:}];
 %edges = linspace(min(holdStarts), max(holdStarts), 100);
 %trsPerMin = histc(edges, holdStarts);
 hSDiffsSec = diff(holdStarts)/1000;
@@ -181,15 +221,11 @@ trsPerMin = trsPerSec*60;
 xs = 1:length(hSDiffsSec);
 
 h1 = plot(xs, trsPerMin);
-set(h1, 'LineStyle', 'none', ...
-        'Marker', 'x', ....
-        'Color', 'b');
-
+set(h1, 'LineStyle', 'none', 'Marker', 'x', 'Color', 'b');
 hold on;
 %pH2 = plot(smooth(trsPerMin, 50, 'loess'));
-pH2 = plot(smooth1(trsPerMin, 'gauss', [2], 3));
-set(pH2, 'Color', 'k', ...
-         'LineWidth', 3);
+pH2 = plot(smooth1(trsPerMin, 'gauss', [8], 16));
+set(pH2, 'Color', 'k','LineWidth', 3);
 
 
 % line
@@ -204,52 +240,71 @@ ylabel('Trials per min');
 nDiffs = length(hSDiffsSec);
 fN = max(1, nDiffs-5);  % if first 6 trials, start at 1
 title(sprintf('Last 6 (sec): %s', mat2str(round(hSDiffsSec(fN:end)))));
-%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 6 - hold times over time
+
 axH = subplot(spSz{:}, 8);
 hold on;
 %hH(1) = plot(smooth(holdV, 50, 'loess'));
 %hH(2) = plot(smooth(holdV, 250, 'loess'));
 hH(1) = plot(smooth1(holdV, 'gauss', [2], 3));
-hH(2) = plot(smooth1(holdV, 'gauss', [2], 3));
-set(hH(2), 'Color', 'k', ...
-           'LineWidth', 3);
+hH(2) = plot(smooth1(holdV, 'gauss', [8], 16));
+set(hH(2), 'Color', 'k', 'LineWidth', 3);
 hRange = [0 prctile(holdV, 95)];
 set(gca, 'YLim', hRange);
 xlabel('trial number');
-ylabel('Hold time (ms)');
-if nDiffs>0
-  totalElapsedS = (input.holdStartsMs{end} - ...
-                   input.holdStartsMs{1})/1000;
+ylabel('hold time (ms)');
+if nDiffs > 0
+  totalElapsedS = (input.holdStartsMs{end} - input.holdStartsMs{1})/1000;
 
   %totalRewMs = sum([input.totalRewardTimesMs{successIx}]);
   totalRewMs = sum(cat(2,input.juiceTimesMsCell{:}));
-  title(sprintf('Total elapsed: %dmin; reward %.1fsec', ...
+  title(sprintf('Total elapsed: %d min; reward %.1fsec', ...
                 round(totalElapsedS/60), totalRewMs/1000));
 end
-%%%%%%%%%%%%%%%%
+plotIntervalLines(input, 'horizontal');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Add a save button
 
 outName = sprintf('/Users/histed/behav-output/%s-behav-i%03d.pdf', ...
                   datestr(now, 'yymmdd'), input.subjectNum);
-epParams = { figNum, outName, ...
-             'FileFormat', 'pdf', ...
-             'Size', [12 12], ...
-             'PrintUI', false };
+epParams = { figNum, outName, 'FileFormat', 'pdf', ...
+             'Size', [12 12], 'PrintUI', false };
 bH = uicontrol(figNum, 'Style', 'pushbutton', ...
                'String', sprintf ('Save PDF figure : %s', outName), ...
-               'Units', 'pixels', ...
-               'Position', [5 5 450 20], ...
-               'Callback', { @saveButtonCb, epParams });
+               'Units', 'pixels', 'Position', [5 5 450 20], ...
+               'Callback', {@saveButtonCb, epParams});
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % subfunctions
 
+function plotIntervalLines(input, orientation)
+
+plotOneIntervalLine(input.tooFastTimeMs, 'k:', orientation);
+plotOneIntervalLine(input.react1TimeMS, 'b:', orientation);
+plotOneIntervalLine(input.react1TimeMS + input.react1DurMS, 'b:', orientation);
+plotOneIntervalLine(input.react2TimeMS, 'r:', orientation);
+plotOneIntervalLine(input.react2TimeMS + input.react2DurMS, 'r:', orientation);
+
+function plotOneIntervalLine(value, lineValue, orientation);
+
+if ~isempty(value)
+	if strcmp(orientation, 'vertical')
+		plot(value * [1 1], get(gca, 'YLim'), lineValue);
+	elseif strcmp(orientation, 'horizontal')
+		plot(get(gca, 'XLim'), value * [1 1], lineValue);
+	else 
+		display(sprintf('plotOneIntervalLine: unknow orientation %s'));
+	end
+end
+
 function saveButtonCb(hObject, eventdata, epParamsIn) 
 exportfigPrint(epParamsIn{:});
 
 %%%%%%%%%%%%%%%%
+
 
